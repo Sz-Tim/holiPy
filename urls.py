49510c16acb5da06eae042a9f57dd77aa575c9c8
Sz-Tim <tim.szewczyk@gmail.com>
2017-05-24 16:20:34 -0600

    trying to deploy....

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+#import hello.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
@@ -11,7 +11,7 @@ import hello.views
 
 urlpatterns = [
     url(r'^$', 'path.to.function'),
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+#    url(r'^$', hello.views.index, name='index'),
+#    url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
